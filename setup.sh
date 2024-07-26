c4bf3f2a87de304ea933243e6f21f281fa5f9f05
Federico Rodriguez <federf2018@live.com>
2024-07-26 13:05:18 -0300

    feat!: added manifests installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NAMESPACE="tcp-ip"
+VALUES_FILE="config/lokistack-values.yaml"
+
 kubeconfig(){
     export KUBECONFIG=~/config/kubeconfig
     kubectl config use-context default
@@ -21,3 +24,50 @@ sudo chmod 777 ~/.kube/config
 export KUBECONFIG=~/.kube/config
 
 echo -e "Installation finished!"
+
+kubectl create namespace $NAMESPACE
+
+kubectl apply -f manifest/emqx
+
+kubectl apply -f manifest/influxdb
+
+kubectl apply -f manifest/mysql
+
+kubectl apply -f manifest/streamlit
+
+kubectl apply -f manifest/telegraf
+
+echo -e "Grafana's Installation\n"
+
+# Add Helm repo and update
+{
+    helm repo add grafana https://grafana.github.io/helm-charts
+    helm repo update
+
+    # Create Kubernetes namespace
+    kubectl create namespace $NAMESPACE
+
+    # Install/upgrade Loki stack
+    helm upgrade --install loki grafana/loki-stack --namespace=$NAMESPACE --values $VALUES_FILE
+
+    # Wait for the Loki Grafana deployment to be ready
+    kubectl rollout status deployment loki-grafana -n $NAMESPACE --timeout=210s
+} > /dev/null 2>&1
+
+# Check if the previous block executed successfully
+if [ $? -eq 0 ]; then
+    echo -e "The Loki stack has been deployed to your cluster. Loki can now be added as a datasource in Grafana."
+else
+    echo -e "An error occurred during the deployment of the Loki stack. Please check the logs for more details."
+    exit 1
+fi
+
+# Retrieve and display Grafana admin password
+echo -e "Admin's Password"
+kubectl get secret loki-grafana --namespace=$NAMESPACE -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+# Instructions to access Grafana service
+echo -e "\nTo access the Grafana service, open a new terminal and execute the following command:"
+echo "kubectl port-forward --namespace $NAMESPACE service/loki-grafana 3000:80"
+echo -e "\nThen, open your browser and go to:"
+echo "http://localhost:3000"
