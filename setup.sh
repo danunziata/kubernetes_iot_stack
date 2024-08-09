1b55afc8dfe351ed812c4bd001b7a4d2aee57e55
Federico Rodriguez <federf2018@live.com>
2024-08-09 14:17:31 -0300

    feat: added rootless mode information

--- setup.sh
+++ setup.sh
@@ -3,111 +3,134 @@
 NAMESPACE="tcp-ip"
 VALUES_LOKI="config/lokistack-values.yaml"
 
-# Helm
 
-echo -e "\nYou are installing Helm..."
+echo -e "\nPlease select one of the following options"
+echo -e "\n[1] K3s Default"
+echo -e "[2] K3s Rootless Mode"
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-rm -rf get_helm.sh
 
-kubeconfig(){
-    export KUBECONFIG=~/config/kubeconfig
-    kubectl config use-context default
-    kubectl get node -o wide
-}
+read -p "Option: " option
 
-# k3s
+valid_option=false
 
-echo -e "\nYou are installing k3s..."
+while [ "$valid_option" == false ]; do
+    case $option in
+        1)
+            # Helm
 
-sudo mkdir -p /etc/rancher/k3s
-sudo cp config/config-k3s.yaml /etc/rancher/k3s/config.yaml
-curl -sfL https://get.k3s.io | sh -
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo chmod 666 ~/.kube/config
-export KUBECONFIG=~/.kube/config
-sleep 10
+            echo -e "\nYou are installing Helm..."
 
-echo -e "Installation finished!"
+            curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+            chmod 700 get_helm.sh
+            ./get_helm.sh
+            rm -rf get_helm.sh
 
-# Manifests
+            kubeconfig(){
+                export KUBECONFIG=~/config/kubeconfig
+                kubectl config use-context default
+                kubectl get nodes -o wide
+            }
 
-echo -e "\nYou are applying the manifests..."
+            # k3s
 
-kubectl create namespace $NAMESPACE
-kubectl apply -f manifest/emqx
-kubectl apply -f manifest/influxdb
-kubectl apply -f manifest/mysql
-kubectl apply -f manifest/streamlit
-kubectl apply -f manifest/telegraf
-kubectl apply -f manifest/jenkins
+            echo -e "\nYou are installing k3s..."
 
-# Loki-Stack
+            sudo mkdir -p /etc/rancher/k3s
+            sudo cp config/config-k3s.yaml /etc/rancher/k3s/config.yaml
+            curl -sfL https://get.k3s.io | sh -
+            sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+            sudo chmod 666 ~/.kube/config
+            export KUBECONFIG=~/.kube/config
+            sleep 10
 
-echo -e "Grafana's Installation\n"
+            echo -e "Installation finished!"
 
-# Add Helm repo and update
-{
-    helm repo add grafana https://grafana.github.io/helm-charts
-    helm repo update
+            # Manifests
 
-    # Create Kubernetes namespace
-    kubectl create namespace $NAMESPACE
+            echo -e "\nYou are applying the manifests..."
 
-    # Install/upgrade Loki stack
-    helm upgrade --install loki grafana/loki-stack --namespace=$NAMESPACE --values $VALUES_LOKI
+            kubectl create namespace $NAMESPACE
+            kubectl apply -f manifest/emqx
+            kubectl apply -f manifest/influxdb
+            kubectl apply -f manifest/mysql
+            kubectl apply -f manifest/streamlit
+            kubectl apply -f manifest/telegraf
+            kubectl apply -f manifest/jenkins
 
-    # Wait for the Loki Grafana deployment to be ready
-    kubectl rollout status deployment loki-grafana -n $NAMESPACE --timeout=210s
-} > /dev/null 2>&1
+            # Loki-Stack
 
-# Check if the previous block executed successfully
-if [ $? -eq 0 ]; then
-    echo -e "The Loki stack has been deployed to your cluster. Loki can now be added as a datasource in Grafana."
-else
-    echo -e "An error occurred during the deployment of the Loki stack. Please check the logs for more details."
-    exit 1
-fi
+            echo -e "Grafana's Installation\n"
 
-# Retrieve and display Grafana admin password
-echo -e "Admin's Password"
-kubectl get secret loki-grafana --namespace=$NAMESPACE -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+            # Add Helm repo and update
+            {
+                helm repo add grafana https://grafana.github.io/helm-charts
+                helm repo update
 
-# Grype
+                # Create Kubernetes namespace
+                kubectl create namespace $NAMESPACE
 
-echo -e "\nYou are installing Grype..."
+                # Install/upgrade Loki stack
+                helm upgrade --install loki grafana/loki-stack --namespace=$NAMESPACE --values $VALUES_LOKI
 
-curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin
+                # Wait for the Loki Grafana deployment to be ready
+                kubectl rollout status deployment loki-grafana -n $NAMESPACE --timeout=210s
+            } > /dev/null 2>&1
 
-# Kubeseal
+            # Check if the previous block executed successfully
+            if [ $? -eq 0 ]; then
+                echo -e "The Loki stack has been deployed to your cluster. Loki can now be added as a datasource in Grafana."
+            else
+                echo -e "An error occurred during the deployment of the Loki stack. Please check the logs for more details."
+                exit 1
+            fi
 
-echo -e "\nYou are installing Kubeseal..."
+            # Retrieve and display Grafana admin password
+            echo -e "Admin's Password"
+            kubectl get secret loki-grafana --namespace=$NAMESPACE -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
-KUBESEAL_VERSION='0.26.0'
-curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
-tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
-sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+            # Grype
 
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm repo update
-helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
+            echo -e "\nYou are installing Grype..."
 
-# Kubescape
+            curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin
 
-echo -e "\nYou are installing Kubescape..."
+            # Kubeseal
 
-curl -s https://raw.githubusercontent.com/kubescape/kubescape/master/install.sh | /bin/bash
-export PATH=$PATH:/home/<user>/.kubescape/bin 
+            echo -e "\nYou are installing Kubeseal..."
 
-# Velero
+            KUBESEAL_VERSION='0.26.0'
+            curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
+            tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
+            sudo install -m 755 kubeseal /usr/local/bin/kubeseal
 
-echo -e "\nYou are installing Velero..."
+            helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+            helm repo update
+            helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 
-wget https://github.com/vmware-tanzu/velero/releases/download/v1.13.2/velero-v1.13.2-linux-amd64.tar.gz
-tar -xvf velero-v1.13.2-linux-amd64.tar.gz
-sudo mv velero-v1.13.2-linux-amd64/velero /usr/local/bin
-rm -rf velero-v1.13.2-linux-amd64
+            # Kubescape
 
-echo -e "\nInstallation Finished"
+            echo -e "\nYou are installing Kubescape..."
+
+            curl -s https://raw.githubusercontent.com/kubescape/kubescape/master/install.sh | /bin/bash
+            export PATH=$PATH:/home/<user>/.kubescape/bin 
+
+            # Velero
+
+            echo -e "\nYou are installing Velero..."
+
+            wget https://github.com/vmware-tanzu/velero/releases/download/v1.13.2/velero-v1.13.2-linux-amd64.tar.gz
+            tar -xvf velero-v1.13.2-linux-amd64.tar.gz
+            sudo mv velero-v1.13.2-linux-amd64/velero /usr/local/bin
+            rm -rf velero-v1.13.2-linux-amd64
+
+            echo -e "\nInstallation Finished"
+            valid_option=true
+            ;;
+        2)
+            echo -e "\nFor more information, go to: https://github.com/danunziata/kubernetes_iot_stack/blob/docs/docs/rootless_mode.md"
+            ;;
+        *)
+            echo -e "Invalid Option. Please try again."
+            ;;
+    esac
+done
